55e2755cd424ad57810c96709756c25a9eb695d4
Roma-kuznetsov <Roma_kuznecov_2000@mail.ru>
2022-01-18 09:06:27 +0300

    Updates

---
+++ precache-manifest.c7d40c0a7f73439a1b88fc675c6d6821.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbd7f4cb66c3afe46339fc5bb2a03307",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "d5511af72aa9c2c6b9bc",
+    "url": "/my-app/static/css/3.894751b7.chunk.css"
+  },
+  {
+    "revision": "3bd396c7f96029101491",
+    "url": "/my-app/static/css/main.c978100a.chunk.css"
+  },
+  {
+    "revision": "f3310e6551c80faa1e57",
+    "url": "/my-app/static/js/2.923ad03d.chunk.js"
+  },
+  {
+    "revision": "d5511af72aa9c2c6b9bc",
+    "url": "/my-app/static/js/3.c47c0480.chunk.js"
+  },
+  {
+    "revision": "3bd396c7f96029101491",
+    "url": "/my-app/static/js/main.67fada7c.chunk.js"
+  },
+  {
+    "revision": "212ecf1d387ce9312bad",
+    "url": "/my-app/static/js/runtime~main.40846fed.js"
+  },
+  {
+    "revision": "a1bccae6ddba0501cded39ae1cb6aa28",
+    "url": "/my-app/static/media/def.a1bccae6.jpg"
+  },
+  {
+    "revision": "b3221b81661167bc429aea99e2586aee",
+    "url": "/my-app/static/media/preloader.b3221b81.gif"
+  }
+]);
